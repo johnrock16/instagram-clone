241be56d4757c2392552d395ff268e66f2a2a247
João <joao.rocha@atonit.com.br>
2023-03-05 00:19:11 -0300

    small fixes

--- src/js/components/story.jsx
+++ src/js/components/story.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 function Story({image, title}) {
     return(
-        <div className='in-profile__story in-story'>
+        <button className='in-profile__story in-story button--transparent' aria-label={`Story ${title}`}>
             <img src={image.src} alt={image.alt} loading="lazy" width="56" height="56"/>
             <h2>{title}</h2>
-        </div>
+        </button>
     )
 }
 
